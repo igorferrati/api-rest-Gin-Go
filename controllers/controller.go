b89e7aa93f881ee7547b670510c8534b224a26ca
igorferrati <ferrati.igor@gmail.com>
2024-01-03 18:19:04 -0300

    add conexão db

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/igorferrati/api-rest-Gin-Go/database"
 	"github.com/igorferrati/api-rest-Gin-Go/models"
 )
 
@@ -14,4 +17,15 @@ func Saudacao(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"Api diz:": "Eai " + nome + ", tudo bem?",
 	})
+}
+
+func CriaNovoAluno(c *gin.Context) {
+	var aluno models.Aluno
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusOK, aluno)
 }
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/igorferrati/api-rest-Gin-Go/database"
 	"github.com/igorferrati/api-rest-Gin-Go/models"
 	"github.com/igorferrati/api-rest-Gin-Go/routes"
 )
 
 func main() {
+	database.ConectaComBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "Igor Ferrati", CPF: "12345678910", RG: "123456789"},
 		{Nome: "Carla Cartoman", CPF: "12345678910", RG: "123456789"},
